67f1e8b765b3a40d77d132ff738bc6ffbf17ea64
vinayvyaslove-ui <vinayvyaslove@gmail.com>
2025-11-29 18:52:23 +0530

    Update new.tsx

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { supabase } from '../../lib/supabaseClient'
+import { supabase } from '../supabaseClient'
 import { useRouter } from 'next/router'
 
 export default function NewInvoice() {
